c001cb37b4ea53fde7bb68f533aeb3e3319f1995
Whyukim <devhyukim@gmail.com>
2023-06-04 21:22:00 +0900

    follow Event

--- front/src/components/Elements/Buttons/Button.tsx
+++ front/src/components/Elements/Buttons/Button.tsx
@@ -2,14 +2,16 @@ interface IButton {
   text: string;
   onClick: () => void;
   red?: boolean;
+  disabled?: boolean;
 }
 
-function Button({ text, onClick, red }: IButton) {
+function Button({ text, onClick, red, disabled = false }: IButton) {
   return (
     <button
       className={`border-none rounded-md py-2 px-8 text-white font-bold ${
         red ? "bg-red-500" : "bg-sky-500"
-      }`}
+      } ${disabled && "opacity-80"}`}
+      onClick={onClick}
     >
       {text}
     </button>
