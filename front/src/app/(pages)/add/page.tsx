0a64b2fe6abe36894f52ea75686104f89b4b9003
Whyukim <devhyukim@gmail.com>
2023-06-05 16:13:28 +0900

    중간저장

--- front/src/app/(pages)/add/page.tsx
+++ front/src/app/(pages)/add/page.tsx
@@ -1,7 +1,23 @@
+import NewPost from "components/Forms/Add/newPost";
+import { Metadata } from "next";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+
+export const metadata: Metadata = {
+  title: "New Post",
+  description: "Create a new post",
+};
+
 interface IaddPage {}
 
-function addPage({}: IaddPage) {
-  return <div>add</div>;
+async function addPage({}: IaddPage) {
+  const session = await getServerSession();
+
+  if (!session?.user) {
+    redirect("/auth/signin");
+  }
+
+  return <NewPost user={session.user} />;
 }
 
 export default addPage;
